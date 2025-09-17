bce6843165ef8d0ab72e9547fbe254f147c54e97
PaulCPPDev <holderbaumpaul@gmail.com>
2025-09-17 18:33:18 +0200

    CLI Args Parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+
+mod urdf_io;
+
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+struct Args{
+    #[arg(long)] urdf: String, //(mandatory) --urdf path_to_urdf
+    #[arg(long)] ee: Option<String>, //(optional) --ee endeffector_name
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    println!("path to urdf = {0}", args.urdf);
 }
